bc3d9ff8948054a07ba7c796e282053930a9ae4d
ramn <github@rymdimperiet.org>
2017-01-16 14:17:19 +0100

    Add crate docs with example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+//! Example usage:
+//!
+//!
+//! ```
+//! $ cargo install repay
+//!
+//! $ repay <<HERE
+//! a 150
+//! b 300
+//! c 100 c a
+//! HERE
+//! c owes b 100.00
+//! a owes b 50.00
+//! ```
+
 extern crate ramn_currency;
 
 use std::io;
